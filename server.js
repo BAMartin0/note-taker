85cb3ff974d7749f34e80a4fca298ce13d41398f
B. Martin <imbrittanyalexis@gmail.com>
2024-06-11 20:05:36 -0400

    completed code

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const path = require('path');
 const db = require('./db/db.json')
+const fs = require('fs')
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const notes = []
 
 app.use(express.static("public"));
+app.use(express.json());
 
 //create html routes
 //index route
@@ -21,24 +24,77 @@ app.get('/notes', (req, res) => {
 
 //get route
 app.get('/api/notes', (req, res) => {
-    res.json(db)
-})
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Error reading notes');
+        }
+        const notes = JSON.parse(data);
+        res.json(notes);
+    });
+});
 
 //post routes
 app.post('/api/notes', (req, res) => {
 
     console.info(`${req.method} request received to add a note`);
 
-    const newNote = res.body; 
+    const newNote = req.body;
 
+    //reads the db file and logs an error if there was one while reading file
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Error reading notes');
+        }
+    //creates notes variable from data read from read file then pushes the data of new note into the file data
+    const notes = JSON.parse(data);
+    notes.push(newNote);
 
-    db.push(newNote); 
+    //rewrites the db file with updated content
+    fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Error saving note');
+        }
+        res.json(newNote);
+    });
+    });
+});
 
-    return res.json(db);
-})
+//delete routes (id params)
 
-//delete routes
+// app.delete('/api/notes/:id', (req, res) => {
 
+//     const noteId = req.params.id;
+
+//     fs.readFile('./db/db.json', 'utf8', (err, data) => {
+//         if (err) {
+//             console.error(err);
+//             return res.status(500).send('Error reading notes');
+//         };
+
+//         let notes = JSON.parse(data);
+
+//         const filteredNotes = [];
+//         for (let i = 0; i < notes.length; i++) {
+//           if (notes[i].id !== noteId) {
+//             filteredNotes.push(notes[i]);
+//           }
+//         }
+//         notes = filteredNotes;
+
+//         fs.readFile('./db/db.json', JSON.stringify(notes), (err) => {
+//             if (err) {
+//               console.error(err);
+//               return res.status(500).send("Error deleting note");
+//             }
+//             res.json(notes);
+//         })
+//     });
+
+    
+// });
 
 //listen() method is responsible for listening for incoming connections on the specified port
 
